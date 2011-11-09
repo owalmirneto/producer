0c7098e22bf86088e73aa451a30cc53dca887495
Walmir Neto <wfsneto@gmail.com>
2011-11-09 02:42:20 -0300

    i18n e l10n sexta aula finalizada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   respond_to :json, :html, :xml
-end
+  before_filter :change_locale
 
+protected 
+  def change_locale
+    @locale = (params[:locale] || session[:locale] || :en)
+    I18n.locale = session[:locale] = @locale
+  end
+end
