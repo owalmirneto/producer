1fe79f06e0bb7f4ef3e34c8492c48340236bede6
Walmir Neto <wfsneto@gmail.com>
2012-01-21 06:07:14 -0300

    alteração do seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-
 ActiveRecord::Base.transaction do
 
   User.create!(
@@ -10,8 +9,8 @@ ActiveRecord::Base.transaction do
 
   1.upto 10 do |i|
     Asset.create!(
-    :title => "Image #{i}" # ,
-    # :document => File.open(Rails.root.join("public/images/asset#{i}.png"))
+    :title => "Image #{i}",
+    :document => File.open(Rails.root.join("public/images/asset#{i}.png"))
     )
   end
 
@@ -26,8 +25,28 @@ ActiveRecord::Base.transaction do
   :draft        => false,
   :published_at => Time.now,
 
-  :excerpt => "High off Baltimore Pandemic and Yellow Tops, I believe we promised a release candidate shortly after RailsConf. As things usually go in open source, we gorged ourselves on fixes and improvements instead.",
+  :excerpt => %{
+    High off Baltimore Pandemic and Yellow Tops,
+    I believe we promised a release candidate shortly after RailsConf.
+    As things usually go in open source, we gorged ourselves on fixes and improvements instead.
+  },
 
-  :body    => "<p>High off Baltimore Pandemic and Yellow Tops, I believe we promised a release candidate shortly after RailsConf. As things usually go in open source, we gorged ourselves on fixes and improvements instead. But all to your benefit. We've had '842 commits by 125 authors':http://github.com/rails/rails/compare/v3.0.0.beta4...master since the release of the last beta!</p> <p>Now it's time to just say good is good enough, otherwise we could keep on with this forever. So please welcome the Rails 3 release candidate! You install, as always, with *gem install rails --pre*.</p> <p>Most of the fixes have been of minor significance, but we did manage to dramatically speed up Rails 3 development and startup speed for larger applications (Basecamp went from insufferable to about 2.3 levels of enjoyment).</p> <p>Speed is now pretty good across the board except for part of Arel that Active Record now depends on. We'll be making sure we get performance of Active Record back to at least 2.3 levels before release.</p>"
+  :body    => %{
+    <p>High off Baltimore Pandemic and Yellow Tops, I believe we promised a release candidate shortly after RailsConf.
+    As things usually go in open source, we gorged ourselves on fixes and improvements instead.
+    But all to your benefit. We’ve had "842 commits by 125 authors":http://github.com/rails/rails/compare/v3.0.0.beta4...master
+    since the release of the last beta!</p>
+
+    <p>Now it’s time to just say good is good enough, otherwise we could keep on with this forever.
+    So please welcome the Rails 3 release candidate!
+    You install, as always, with *gem install rails --pre*.</p>
+
+    <p>Most of the fixes have been of minor significance, but we did manage to dramatically speed up Rails 3
+    development and startup speed for larger applications
+    (Basecamp went from insufferable to about 2.3 levels of enjoyment).</p>
+
+    <p>Speed is now pretty good across the board except for part of Arel that Active Record now depends on.
+    We’ll be making sure we get performance of Active Record back to at least 2.3 levels before release.</p>
+  }
   )
 end
