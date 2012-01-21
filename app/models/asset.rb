988b4f054ee7607dc404a627bdb0ee502099f09e
Walmir Neto <wfsneto@gmail.com>
2012-01-21 00:04:35 -0300

    mini ajustes

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,14 +1,18 @@
 class Asset < ActiveRecord::Base
 
+  # validação de campo
   validates_presence_of :title
 
+  # dimensões dos thumbs a ser salvo
   has_attached_file :document, :styles => {
     :medium => "300x300#",
     :thumb  => "50x50#"
   }
 
   validates_attachment_presence     :document
+  # tamanho do file
   validates_attachment_size         :document, :less_than => 5.megabyte
+  # cabeçalhos aceitos pelo paperclip
   validates_attachment_content_type :document, :content_type => %w(image/png image/jpeg image/pjpeg image/gif image/tiff)
 
 end
