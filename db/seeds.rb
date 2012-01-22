c15ed2e20e2a5b6db728ef17dd83470e9034c4d1
Walmir Neto <wfsneto@gmail.com>
2012-01-22 11:22:18 -0300

    Primeira parte da 8ª - Formulário de contato e envio de email

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,10 +41,10 @@ ActiveRecord::Base.transaction do
     :body    => %{
       <p>High off Baltimore Pandemic and Yellow Tops, I believe we promised a release candidate shortly after RailsConf.
       As things usually go in open source, we gorged ourselves on fixes and improvements instead.
-      But all to your benefit. We’ve had "842 commits by 125 authors":http://github.com/rails/rails/compare/v3.0.0.beta4...master
+      But all to your benefit. We've had "842 commits by 125 authors":http://github.com/rails/rails/compare/v3.0.0.beta4...master
       since the release of the last beta!</p>
 
-      <p>Now it’s time to just say good is good enough, otherwise we could keep on with this forever.
+      <p>Now it's time to just say good is good enough, otherwise we could keep on with this forever.
       So please welcome the Rails 3 release candidate!
       You install, as always, with *gem install rails --pre*.</p>
 
@@ -53,7 +53,7 @@ ActiveRecord::Base.transaction do
       (Basecamp went from insufferable to about 2.3 levels of enjoyment).</p>
 
       <p>Speed is now pretty good across the board except for part of Arel that Active Record now depends on.
-      We’ll be making sure we get performance of Active Record back to at least 2.3 levels before release.</p>
+      We'll be making sure we get performance of Active Record back to at least 2.3 levels before release.</p>
     }
   )
 end
