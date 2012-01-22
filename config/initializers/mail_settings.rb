387f090d7578013403f39865a925f1de865a6fda
Walmir Neto <wfsneto@gmail.com>
2012-01-22 10:52:32 -0300

    Primeira parte da 8ª - Formulário de contato e envio de email

---
+++ config/initializers/mail_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 465,
+  :domain               => "wfsneto.com.br",
+  :user_name            => "mail",
+  :password             => "s3nh49Y4bYJ>rf4c1l",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
